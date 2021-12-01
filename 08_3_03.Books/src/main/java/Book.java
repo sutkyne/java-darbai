144afe1dc18b1483ebe649525276d0c6e9faa205
Justas N <justasutkynesss@gmail.com>
2021-12-01 22:05:10 +0200

    strigau su contains

--- 08_3_03.Books/src/main/java/Book.java
+++ 08_3_03.Books/src/main/java/Book.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Book {
 
@@ -16,5 +17,28 @@ public class Book {
     public int getPublicationYear() {
         return publicationYear;
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, publicationYear);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(name, other.name) && publicationYear == other.publicationYear;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", publicationYear=" + publicationYear + "]";
+	}
+	
 
 }
--- 08_3_03.Books/src/main/java/Main.java
+++ 08_3_03.Books/src/main/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Book> books = new ArrayList<>();
+        boolean result = false;
+
 
         while (true) {
             System.out.println("Name (empty will stop):");
@@ -18,10 +20,20 @@ public class Main {
             System.out.println("Publication year:");
             int publicationYear = Integer.valueOf(scanner.nextLine());
             Book book = new Book(name, publicationYear);
+            //result=!(books.contains(book.getName() )&&books.contains(book.getPublicationYear()));
+            result=book instanceof books;
             books.add(book);
+//            if (books.isEmpty()) {
+//            	books.add(book);
+//			} else if (result){
+//        	   books.add(book);
+//           }
 
         }
-        
+        for (Book book : books) {
+			System.out.println(book);
+		}
+
         // NB! Don't alter the line below!
         System.out.println("Thank you! Books added: " + books.size());
     }
