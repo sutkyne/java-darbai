4ab701a7733b98fd13737937e4baa0dfd9bb2dcd
Justas N <justasutkynesss@gmail.com>
2021-12-09 14:25:37 +0200

    finished 12

--- 08_3_12.CargoHold/src/main/java/Hold.java
+++ 08_3_12.CargoHold/src/main/java/Hold.java
@@ -1,29 +1,32 @@
 import java.util.ArrayList;
 
 public class Hold {
-private int maxWeight;
-private ArrayList<Suitcase> sCase = new ArrayList<>();
-private int totlWeight=0;
+	private int maxWeight;
+	private ArrayList<Suitcase> sCase = new ArrayList<>();
+	private int totlWeight = 0;
 
-public Hold(int maxWeight) {
-	super();
-	this.maxWeight = maxWeight;
-}
-public void addSuitcase(Suitcase suitcase) {
-	totlWeight += suitcase.totalWeight();
+	public Hold(int maxWeight) {
+		super();
+		this.maxWeight = maxWeight;
+	}
 
-	if (totlWeight <= maxWeight) {
-		sCase.add(suitcase);
+	public void addSuitcase(Suitcase suitcase) {
+		totlWeight += suitcase.totalWeight();
+
+		if (totlWeight <= maxWeight) {
+			sCase.add(suitcase);
+
+		}
 
 	}
-	
-}
-public void printItems() {
-	for (Suitcase s : sCase) {
-		System.out.println(s);
+
+	public void printItems() {
+		for (Suitcase s : sCase) {
+			s.printItems();
+		}
 	}
-}
-public String toString() {
-	return sCase.size()+" suitcases ("+totlWeight +" kg)";
-}
+
+	public String toString() {
+		return sCase.size() + " suitcases (" + totlWeight + " kg)";
+	}
 }
