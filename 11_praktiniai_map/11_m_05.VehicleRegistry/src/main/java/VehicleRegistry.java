a8b20dd52f05ce7c9c1379f076794888dbd44598
Justas N <justasutkynesss@gmail.com>
2022-01-12 09:07:15 +0200

    init commit

--- 11_praktiniai_map/11_m_05.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ 11_praktiniai_map/11_m_05.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,9 +1,15 @@
 
 public class VehicleRegistry {
+
 	public boolean add(LicensePlate licensePlate, String owner) {
 
 	}
+
 	public String get(LicensePlate licensePlate) {
+
+	}
+
+	public boolean remove(LicensePlate licensePlate) {
 
 	}
 }
--- 9_Acc_2/src/AccountSavings.java
+++ 9_Acc_2/src/AccountSavings.java
@@ -1,5 +1,5 @@
 
-public class AccountSavings extends Account{
+public class AccountSavings extends Account {
 
 	public AccountSavings() {
 		super();
@@ -8,16 +8,17 @@ public class AccountSavings extends Account{
 
 	public AccountSavings(int id, double balance, double annualInterestRate) {
 		super(id, balance, annualInterestRate);
-		
+
 	}
+
 	public AccountSavings(AccountWithTransaction a) {
 		super(a.getId(), a.getBalance(), a.getAnnualInterestRate());
-	
+
 	}
 
 	public AccountSavings(int id, double balance) {
 		super(id, balance);
-	
+
 	}
 
 	@Override
