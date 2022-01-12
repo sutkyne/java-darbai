972f251e5031332ce523ef5412f9be9667f8231e
Justas N <justasutkynesss@gmail.com>
2022-01-12 12:16:57 +0200

    init commit

--- 11_praktiniai_map/11_m_05.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ 11_praktiniai_map/11_m_05.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,15 +1,44 @@
+import java.util.HashMap;
 
 public class VehicleRegistry {
+	private HashMap<LicensePlate, String> owners = new HashMap<>();
 
 	public boolean add(LicensePlate licensePlate, String owner) {
+		if (owners.containsKey(licensePlate)) {
+			return false;
+		} else {
+			owners.put(licensePlate, owner);
+			return true;
+
+		}
 
 	}
 
 	public String get(LicensePlate licensePlate) {
-
+		return owners.get(licensePlate);
 	}
 
 	public boolean remove(LicensePlate licensePlate) {
+		if (owners.containsKey(licensePlate)) {
+			owners.remove(licensePlate);
+			return true;
 
+		}
+
+		return false;
+	}
+
+	public void printLicensePlates() {
+		owners.forEach((k, v) -> System.out.println(k));
+	}
+
+	public void printOwners() {
+		HashMap<LicensePlate, String> ownersTemp = new HashMap<>();
+		owners.forEach((k, v) -> {
+			if (owners.containsValue(v) != ownersTemp.containsValue(v)) {
+				System.out.println(v);
+				ownersTemp.put(k, v);
+			}
+		});
 	}
 }
