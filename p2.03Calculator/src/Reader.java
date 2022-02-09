b070f3a254c3cb67a6ccaf5ea34c9938ee18f918
Justas Nominaitis <justas.nominaitis@gmail.com>
2022-02-09 17:37:00 +0200

    commit with no errors..I hope

--- p2.03Calculator/src/Reader.java
+++ p2.03Calculator/src/Reader.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 public class Reader {
- private Scanner sc;
+ private Scanner sc=new Scanner(System.in);;
 public String readString() {
-	sc=new Scanner(System.in);
 	String reading = sc.nextLine();
 	return reading;
 }
 public int readInteger() {
-	sc=new Scanner(System.in);
 	int readInt = Integer.parseInt(sc.nextLine());
 	return readInt;
 }
